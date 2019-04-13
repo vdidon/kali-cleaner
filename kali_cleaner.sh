59cc50987b39cdce964328562a999b3d1e11586c
vdidon <vdidon@live.fr>
2019-04-13 12:23:31 +0200

    Remove redundant statements

--- kali_cleaner.sh
+++ kali_cleaner.sh
@@ -15,9 +15,6 @@ CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
 LINUXPKG="linux-(image|headers|debian-modules|restricted-modules)"
 METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
 #OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
-YELLOW="\033[1;33m"
-RED="\033[0;31m"
-ENDCOLOR="\033[0m"
  
 if [ $USER != root ]; then
 echo -e $RED"[Kali-cleaner]:Error: must be root"
